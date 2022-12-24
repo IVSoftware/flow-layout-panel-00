881604be3074ae206ca4482f76f56eecd00baa5b
Tom Gregor <tgregor@ivsoftware.com>
2022-12-24 05:57:42 -0700

    Add project files.

---
+++ table-layout-panel-00/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace table_layout_panel_00
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel.Location = new System.Drawing.Point(167, 12);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(260, 220);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+    }
+}
+
---
+++ table-layout-panel-00/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace table_layout_panel_00
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            populateFlowLayoutPanel();
+        }
+
+        private void populateFlowLayoutPanel()
+        {
+            flowLayoutPanel.AutoScroll = true;
+            // https://stackoverflow.com/a/3205512/5438626
+            int usableWidth =
+                flowLayoutPanel.Width - SystemInformation.VerticalScrollBarWidth;
+            List<TextBox> tmp = new List<TextBox>();
+            for (int i = 0; i < 25; i++)
+            {
+                var textBox =
+                new TextBox
+                {
+                    Text = $"TextBox{i}",
+                    Name = $"textBox{i}",
+                    Margin = new Padding(),
+                    Width = usableWidth,
+                    Anchor = AnchorStyles.Left | AnchorStyles.Right,
+                };
+                textBox.TextChanged += onAnyTextChanged;
+                flowLayoutPanel.Controls.Add(textBox);
+                tmp.Add(textBox);
+            }
+            Textboxes = tmp.ToArray();
+        }
+        // Handle text changes in any box
+        private void onAnyTextChanged(object sender, EventArgs e)
+        {
+            if(sender is TextBox textbox)
+            {
+                // Do something with the text box that changed.                
+            }
+        }
+        // Access an individual textboxes easily by index
+        TextBox[] Textboxes;
+    }
+}
